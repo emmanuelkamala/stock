ee8d8643affeafb0cf31eb5b712dff248c429c65
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-05-31 23:45:45 +0300

    Edit devise style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(user)
+    dashboard_path
+  end
 end
--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,8 @@
 class PublicController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+  
   def index
-    
+
   end
 
   def dashboard
