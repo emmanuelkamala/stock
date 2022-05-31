e7d233c82932010e52fd5c3881fdd8f65f9d3adc
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-06-01 00:10:22 +0300

    Add association

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,12 +1,15 @@
 class PublicController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
-  
+
   def index
 
   end
 
   def dashboard
     @title = 'Dashboard'
+    @incomes = Income.all
+    @expenses = Expense.all
+    @flocks = Flock.all
   end
 
   def financial
