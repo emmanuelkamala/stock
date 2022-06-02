efa69b84aad90f5d8b4d89b1de0fa7cb6566292d
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-06-02 17:37:16 +0300

    Edit styles

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,8 +1,8 @@
 class PublicController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
-  def index
-
+  def report
+    @title = 'Report'
   end
 
   def dashboard
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
-  root 'public#index'
+  devise_scope :user do
+    get '/register', to: 'devise/registrations#new', as: :register
+    authenticated :user do
+      root 'public#dashboard', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   get '/dashboard', to: 'public#dashboard'
   get '/financial', to: 'public#financial'
   get '/production', to: 'public#production'
-  devise_for :users
+  get '/report', to: 'public#report'
+  devise_for :users, :controllers => { 
+                      :registrations => 'users/registrations'
+                     }
   resources :incomes
   resources :flocks
   resources :expenses
