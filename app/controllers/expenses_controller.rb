9b5f822c9b5b7cd909f9028f2e0e28b1af56a64c
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-06-07 11:44:07 +0300

    Edit incomes controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,73 +1,52 @@
 class ExpensesController < ApplicationController
   before_action :set_expense, only: %i[ show edit update destroy ]
 
-  # GET /expenses or /expenses.json
   def index
     @expenses = Expense.all
     @title = 'All Expenses'
   end
 
-  # GET /expenses/1 or /expenses/1.json
   def show
     @title = "Expense #{@expense.id}"
   end
 
-  # GET /expenses/new
   def new
     @expense = Expense.new
     @title = 'New Expense'
   end
 
-  # GET /expenses/1/edit
   def edit
     @title = "Edit Expense #{@expense.id}"
   end
 
-  # POST /expenses or /expenses.json
   def create
     @expense = Expense.new(expense_params)
 
-    respond_to do |format|
-      if @expense.save
-        format.html { redirect_to expense_url(@expense), notice: "Expense was successfully created." }
-        format.json { render :show, status: :created, location: @expense }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @expense.errors, status: :unprocessable_entity }
-      end
+    if @expense.save
+      redirect_to expense_url(@expense), notice: "Expense was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /expenses/1 or /expenses/1.json
   def update
-    respond_to do |format|
-      if @expense.update(expense_params)
-        format.html { redirect_to expense_url(@expense), notice: "Expense was successfully updated." }
-        format.json { render :show, status: :ok, location: @expense }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @expense.errors, status: :unprocessable_entity }
-      end
+    if @expense.update(expense_params)
+      redirect_to expense_url(@expense), notice: "Expense was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /expenses/1 or /expenses/1.json
   def destroy
     @expense.destroy
-
-    respond_to do |format|
-      format.html { redirect_to expenses_url, notice: "Expense was successfully destroyed." }
-      format.json { head :no_content }
-    end
+      redirect_to expenses_url, notice: "Expense was successfully destroyed." 
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_expense
       @expense = Expense.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def expense_params
       params.require(:expense).permit(:date, :category, :quantity, :unit_price, :total_amount, :description, :type_id)
     end
--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,73 +1,52 @@
 class IncomesController < ApplicationController
   before_action :set_income, only: %i[ show edit update destroy ]
 
-  # GET /incomes or /incomes.json
   def index
     @incomes = Income.all
     @title = 'Incomes'
   end
 
-  # GET /incomes/1 or /incomes/1.json
   def show
     @title = "Income #{@income.id}"
   end
 
-  # GET /incomes/new
   def new
     @income = Income.new
     @title = "New Income"
   end
 
-  # GET /incomes/1/edit
   def edit
     @title = "Edit Income #{@income.id}"
   end
 
-  # POST /incomes or /incomes.json
   def create
     @income = Income.new(income_params)
 
-    respond_to do |format|
-      if @income.save
-        format.html { redirect_to income_url(@income), notice: "Income was successfully created." }
-        format.json { render :show, status: :created, location: @income }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @income.errors, status: :unprocessable_entity }
-      end
+    if @income.save
+      redirect_to income_url(@income), notice: "Income was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /incomes/1 or /incomes/1.json
   def update
-    respond_to do |format|
-      if @income.update(income_params)
-        format.html { redirect_to income_url(@income), notice: "Income was successfully updated." }
-        format.json { render :show, status: :ok, location: @income }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @income.errors, status: :unprocessable_entity }
-      end
+    if @income.update(income_params)
+      redirect_to income_url(@income), notice: "Income was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /incomes/1 or /incomes/1.json
   def destroy
     @income.destroy
-
-    respond_to do |format|
-      format.html { redirect_to incomes_url, notice: "Income was successfully destroyed." }
-      format.json { head :no_content }
-    end
+      redirect_to incomes_url, notice: "Income was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_income
       @income = Income.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def income_params
       params.require(:income).permit(:date, :category, :quantity, :unit_price, :total_amount, :description, :type_id)
     end
