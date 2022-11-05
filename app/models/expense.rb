2aeebe96ebb247fe70bf7eb03efbe21448ada548
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-11-06 00:55:24 +0300

    Edit expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -8,4 +8,16 @@ class Expense < ApplicationRecord
   def total
     unit_price.to_i * quantity.to_i
   end
+
+  def self.to_csv
+    attributes = Expense.column_names
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |expense|
+        csv << attributes.map{ |attr| expense.send(attr) }
+      end
+    end
+  end
 end
