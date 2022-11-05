f64907327f121fee14d4aa046058063c4d666f22
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-11-06 00:56:07 +0300

    Edit fixed expense

--- app/models/fixed_expense.rb
+++ app/models/fixed_expense.rb
@@ -6,4 +6,16 @@ class FixedExpense < ApplicationRecord
   def total
     cost.to_i * quantity.to_i
   end
+
+  def self.to_csv
+    attributes = FixedExpense.column_names
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |fixed_expense|
+        csv << attributes.map{ |attr| fixed_expense.send(attr) }
+      end
+    end
+  end
 end
