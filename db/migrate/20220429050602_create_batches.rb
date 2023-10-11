99f8f8f4f72a2d13d569dbfd6e81162390638011
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2023-10-11 15:33:26 +0300

    Edit migration

---
+++ db/migrate/20220429050602_create_batches.rb
@@ -0,0 +1,11 @@
+class CreateBatches < ActiveRecord::Migration[7.0]
+    def change
+      create_table :batches do |t|
+        t.integer :batch_no
+        t.string :flock_type
+  
+        t.timestamps
+      end
+    end
+  end
+  
